fc76d0d3c940c2b02c60f7b8b41bcf6bb26602a9
VanLance <dylan23smith1990@gmail.com>
2025-05-29 07:09:10 -0500

    factories

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -5,10 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Job extends Model {
-    protected $table = "job_listings";
-
+class Employer extends Model
+{
     use HasFactory;
-
-    protected $fillable = ["title", "salary"];
+    
 }
