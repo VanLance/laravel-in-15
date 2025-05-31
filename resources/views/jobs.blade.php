029e00d6156ae10426d48628c9f0671edbbb949a
VanLance <dylan23smith1990@gmail.com>
2025-05-31 11:17:36 -0500

    eager load jobs

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,13 +3,16 @@
         Jobs Page
     </x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}">
-                    <li><strong>{{ $job['title'] }}</strong>: Pays {{ $job["salary"] }} per year.</li>
-                </a>
-            </li>
+            <a href="/jobs/{{ $job['id'] }}" class="hover:underline block px-4 py-6 border border-gray-200">
+                
+                <div class="font-bold text-blue-500 text-sm ">{{ $job->employer->name }}</div>
+
+                <div>
+                    <strong>{{ $job['title'] }}</strong>: Pays {{ $job["salary"] }} per year.
+                </div>
+            </a>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
